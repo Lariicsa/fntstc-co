b2ea5f364a312e7533b8050c4c3e6e145cbd8e4d
Larissa <larissa.avila@tucanton.com>
2020-02-16 16:26:20 -0600

    Add finder component

---
+++ src/components/Finder.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="finder">
+    <input type="text" value="Buscar" />
+    <span class="finder-icon"></span>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../scss/index.scss";
+@import "../scss/shared/_mixins.scss";
+
+.finder {
+  background-color: $snow;
+  border: 0.1rem solid $border-grey;
+  border-radius: 0.8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4.6rem;
+  width: 100%;
+  max-width: 44rem;
+  &-icon {
+    @include icon-set("finder");
+    background-size: 28%;
+    background-position: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 4.6rem;
+    width: 5.3rem;
+  }
+  input {
+    display: block;
+    border: none;
+    border-top-left-radius: 0.8rem;
+    border-bottom-left-radius: 0.8rem;
+    padding: 1rem 1.6rem;
+    width: calc(100% - 53px);
+    height: 100%;
+  }
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="space" id="app">
+    <Header></Header>
+    <div class="container">
+      <div class="row center">
+        <h2>Visita sin costo las casa que más te gusten</h2>
+      </div>
+      <div class="row center">
+        <p>Elige la fecha y en qué momento del día:</p>
+      </div>
+      <div class="container">
+        <div class="row">
+
+        </div>
+        <div class="row center">
+          <ul class="row center">
+            <OptionButton name="options" :checked="false"  @change="changeValue">Por la mañana</OptionButton>
+            <OptionButton :checked="true" name="options"  @change="changeValue">Por la tarde</OptionButton>
+          </ul>
+      </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <SendForm />
+        </div>
+        <div class="row center">
+          <p>Al agendar, estás aceptando nuestro <a href="https://www.tucanton.com/avisoPrivacidad">Aviso de privacidad</a></p>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
-  name: 'Home',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+  data() {
+    return {
+      selectedValue: ''
+    };
+  },
+  methods: {
+    changeValue: function(newValue) {
+      this.selectedValue = newValue;
+    }
   }
-}
+};
 </script>
